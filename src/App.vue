087b41b1fffc1f5913a7500de3030b6c04017572
giangvincent <giang.dlinh92@gmail.com>
2023-02-14 09:03:07 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNav />
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+import SiteNav from "./components/SiteNav.vue";
+export default {
+  setup() {
+    return {};
+  },
+  components: { SiteNav },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
