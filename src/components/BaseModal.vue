4647b0e971879af8b6b539390c2d250977aa1beb
giangvincent <giang.dlinh92@gmail.com>
2023-02-25 20:37:20 +0700

    feat: add base modal to project

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="absolute top-0 left-0 flex justify-center w-full h-screen px-0 bg-black  bg-opacity-30"
+  >
+    <div class="self-start max-w-screen-md p-4 mt-32 bg-white">
+      <slot />
+      <button class="px-6 py-2 mt-8 text-white bg-weather-primary">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -1,33 +1,36 @@
 <template>
-  <header class="sticky top-0 bg-weather-primary shadow-lg">
+  <header class="sticky top-0 shadow-lg bg-weather-primary">
     <nav
-      class="
-        container
-        flex flex-col
-        sm:flex-row
-        items-center
-        gap-4
-        text-white
-        py-6
-      "
+      class="container flex flex-col items-center gap-4 py-6 text-white  sm:flex-row"
     >
       <RouterLink :to="{ home }">
-        <div class="flex items-center gap-3 flex-1">
-          <i class="fa-solid fa-sun text-2xl"></i>
+        <div class="flex items-center flex-1 gap-3">
+          <i class="text-2xl fa-solid fa-sun"></i>
           <p class="text-2xl">Local Weather</p>
         </div>
       </RouterLink>
 
-      <div class="flex gap-3">
-        <i class="fa-light fa-circle-info text-2xl"></i>
+      <div class="flex justify-end flex-1 gap-3">
+        <i
+          class="text-xl duration-150 cursor-pointer  fa-solid fa-circle-info hover:text-weather-secondary"
+        ></i>
+        <i
+          class="text-xl duration-150 cursor-pointer  fa-solid fa-plus hover:text-weather-secondary"
+        ></i>
       </div>
+
+      <base-modal>
+        <h1>Hello</h1>
+      </base-modal>
     </nav>
   </header>
 </template>
 
 <script>
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
 export default {
+  components: { BaseModal },
   setup() {
     return {};
   },
